886e8a6b6aa57962527f95a3031b67cd02005a49
MaxKonovalov38 <max.kon.38r@gmail.com>
2025-03-27 23:45:19 +0800

    update file

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+//mod problem140;
+//mod problem210;
+//mod problem211;
+//mod problem212;
+//mod problem213;
+//mod problem214;
+//mod problem215;
+//mod problem216;
+//mod problem217;
+//mod problem218;
+//mod problem219;
+mod problem220;
+
+fn main() {
+//    print!("Problem 1.4-0: ");
+//    problem140::run();
+//    print!("Problem 2.1-0: ");
+//    problem210::run();
+//    print!("Problem 2.1-1: ");
+//    problem211::run();
+//    print!("Problem 2.1-2: ");
+//    problem212::run();
+//    print!("Problem 2.1-3: ");
+//    problem213::run();
+//    print!("Problem 2.1-4: ");
+//    problem214::run();
+//    print!("Problem 2.1-5: ");
+//    problem215::run();
+//    print!("Problem 2.1-6: ");
+//    problem216::run();
+//    print!("Problem 2.1-7: ");
+//    problem217::run();
+//    print!("Problem 2.1-8: ");
+//    problem218::run();
+//    print!("Problem 2.1-9: ");
+//    problem219::run();
+    print!("Problem 2.2-0: ");
+    problem220::run();
+}
